44b904c6c4e303cb6a35bbdb51a679f8a92a4595
guest01 <guest01@10.1.49.100>
2013-09-30 08:54:06 -0300

    Adicionando a classe pessoa

---
+++ src/testegit/Pessoa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testegit;
+
+/**
+ *
+ * @author guest01
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    public Pessoa(){
+        
+    }
+    public String getNome(){
+        return nome;
+    }
+}
