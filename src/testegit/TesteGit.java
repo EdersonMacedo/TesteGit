005bcd419ee36ef06a8fadda6e2205292fafc410
guest01 <guest01@10.1.49.100>
2013-09-30 08:43:48 -0300

    Inicio do projeto.

---
+++ src/testegit/TesteGit.java
@@ -0,0 +1,12 @@
+
+package testegit;
+
+import javax.swing.JOptionPane;
+
+public class TesteGit {
+
+    
+    public static void main(String[] args) {{
+        JOptionPane.showMessageDialog(null, "Teste...");
+    }}
+}
